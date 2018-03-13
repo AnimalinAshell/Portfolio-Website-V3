11d84de002a9a4927b89390fb804676c8beac724
Manny Munoz <munoz.jeffrey95@yahoo.com>
2018-03-13 14:42:13 -0400

    Added animations and favicon

--- resources/js/script.js
+++ resources/js/script.js
@@ -7,9 +7,23 @@ particlesJS.load("particles-js", "resources/js/particles.json", function() {
 
 // Nav sticky
 $(document).ready(function() {
-  var sticky = new Waypoint.Sticky({ element: $(".nav")[0] });
-  sticky.destroy();
-  sticky = new Waypoint.Sticky({ element: $(".nav")[0] });
+  setTimeout(function() {
+    var sticky = new Waypoint.Sticky({ element: $(".nav")[0] });
+    sticky.destroy();
+    sticky = new Waypoint.Sticky({ element: $(".nav")[0] });
+  }, 1000);
+
+  // Content animations that would get set off at the wrong time
+  $("#aboutTitle").waypoint({ handler: function(direction) {
+      $("#aboutTitle").removeClass("hidden");
+      $("#aboutTitle").addClass("animated fadeInLeft");
+    }, offset: "55%" });
+  $("#aboutTitle").waypoint({ handler: function(direction) {
+      $(".headShot, .aboutInfo").removeClass("hidden");
+      $(".headShot").addClass("animated slideInUp");
+      $(".aboutInfo").addClass("animated slideInDown");
+    }, offset: "35%" });
+
 });
 
 // Smooth scroll to content
@@ -18,10 +32,8 @@ $('nav a').on('click',function(e){
   $('html,body').animate({scrollTop:getRelatedContent(this).offset().top - 20});
 });
 
-
-
 // Helper functions
-// Get section or article by href
+// Get div by href
 function getRelatedContent(el){
   return $($(el).attr('href'));
 }
@@ -42,32 +54,80 @@ $(".skills").waypoint({
   handler: function(direction) {
     $(".nav > a").removeClass("active");
     $(".nav > a#skillsNav").addClass("active");
-  }
+  },
+  offset: "18%"
 });
 $(".about").waypoint({
   handler: function(direction) {
     $(".nav > a").removeClass("active");
     $(".nav > a#aboutNav").addClass("active");
-  }
+  },
+  offset: "18%"
 });
 $(".home").waypoint({
   handler: function(direction) {
     $(".nav > a").removeClass("active");
     $(".nav > a#homeNav").addClass("active");
   },
-  offset: '-20%'
+  offset: '-.5%'
 });
 $(".portfolio").waypoint({
   handler: function(direction) {
     $(".nav > a").removeClass("active");
     $(".nav > a#portfolioNav").addClass("active");
-  }
+  },
+  offset: "18%"
 });
 $(".contact").waypoint({
   handler: function(direction) {
     $(".nav > a").removeClass("active");
     $(".nav > a#contactNav").addClass("active");
-  }
+  },
+  offset: "18%"
 });
 
+// Content Animations on scroll
+$("#skillsTitle").waypoint({
+  handler: function(direction) {
+    $("div.skillsContent i").removeClass("hidden");
+    $("div.skillsContent i").addClass("animated flipInY");
+  },
+  offset: "35%"
+});
+$("#skillsTitle").waypoint({
+  handler: function(direction) {
+    $("#skillsTitle").removeClass("hidden");
+    $("#skillsTitle").addClass("animated fadeInRight");
+  },
+  offset: "55%"
+});
+$("#portfolioTitle").waypoint({
+  handler: function(direction) {
+    $(".project").removeClass("hidden");
+    $(".project").addClass("animated fadeInUp");
+  },
+  offset: "35%"
+});
+$("#portfolioTitle").waypoint({
+  handler: function(direction) {
+    $("#portfolioTitle").removeClass("hidden");
+    $("#portfolioTitle").addClass("animated fadeInLeft");
+  },
+  offset: "55%"
+});
+$("#contactTitle").waypoint({
+  handler: function(direction) {
+    $(".contact h5, .contactForm").removeClass("hidden");
+    $(".contact h5").addClass("animated fadeInUp");
+    $(".contactForm").addClass("animated zoomIn");
+  },
+  offset: "35%"
+});
+$("#contactTitle").waypoint({
+  handler: function(direction) {
+    $("#contactTitle").removeClass("hidden");
+    $("#contactTitle").addClass("animated fadeInDown");
+  },
+  offset: "55%"
+});
 
